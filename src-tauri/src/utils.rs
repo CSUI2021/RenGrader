c0c7cb4875b75bae47c505941ae7c90cb8c47af0
Rendy Arya Kemal <renrror@gmail.com>
2022-10-19 17:19:22 +0700

    Use whitespace class instead of space

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::{fs::read_to_string, path::Path};
 
 pub fn get_class_name(source_path: &Path) -> String {
-    let re_space = Regex::new(" {2,}").unwrap();
+    let re_space = Regex::new(r"\s{2,}").unwrap();
     let re = Regex::new(r"public class (.+?) \{").unwrap();
 
     let mut content = read_to_string(source_path).unwrap();
