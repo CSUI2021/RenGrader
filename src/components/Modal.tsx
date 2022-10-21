d3d0504faf018da9155f66cd21aac4e66d1923fe
Rendy Arya Kemal <renrror@gmail.com>
2022-10-21 17:24:24 +0700

    Use div instead of p

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,7 +21,7 @@ export const Modal = ({ title, children, show, onClose }: ModalProps) => {
       <div className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-lg">{title}</h3>
-          <p className="py-4">{children}</p>
+          <div className="py-4">{children}</div>
           <div className="modal-action">
             <label htmlFor="my-modal" className="btn" onClick={onClose}>
               Close
